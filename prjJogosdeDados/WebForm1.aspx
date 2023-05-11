a4980d69770f21ed3210b41a84db5e3b15d0ef2f
Nicholas de Alencar e Silva <102756718+Allen947@users.noreply.github.com>
2023-05-11 15:25:07 -0300

    added

---
+++ prjJogosdeDados/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="prjJogosdeDados.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link rel="stylesheet" type="text/css" href="CSS/estilos.css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image class="img" ID="Image1" runat="server" Height="150px" Width="150px" />
+            <asp:Image class="img" ID="Image2" runat="server" Height="150px" Width="150px" />
+            <br />
+            <asp:Button class="btn" ID="BtnSortear" runat="server" Text="Sortear" OnClick="BtnSortear_Click" /><br />
+            <asp:Label ID="Label1" runat="server" Text="Empates: "></asp:Label>
+            <asp:Label ID="lblempate" runat="server" Text="0"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ prjJogosdeDados/WebForm1.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace prjJogosdeDados
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSortear_Click(object sender, EventArgs e)
+        {
+            Random rdn = new Random();
+            int valor = rdn.Next(1, 7);
+            int valor2 = rdn.Next(1, 7);
+
+            int empate = int.Parse(lblempate.Text);
+
+            if (valor == 1)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image1.jpg";
+
+            }
+            if (valor == 2)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image2.jpg";
+
+            }
+            if (valor == 3)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image3.jpg";
+
+            }
+            if (valor == 4)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image4.jpg";
+
+            }
+            if (valor == 5)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image5.jpg";
+
+            }
+            if (valor == 6)
+            {
+                Image1.ImageUrl = "~/CSS/Image/image6.jpg";
+
+            }
+    //--------------------------------------------------------------------------------------------------------------------------------
+            if (valor2 == 1)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image1.jpg";
+
+            }
+            if (valor2 == 2)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image2.jpg";
+
+            }
+            if (valor2 == 3)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image3.jpg";
+
+            }
+            if (valor2 == 4)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image4.jpg";
+
+            }
+            if (valor2 == 5)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image5.jpg";
+
+            }
+            if (valor2 == 6)
+            {
+                Image2.ImageUrl = "~/CSS/Image/image6.jpg";
+
+            }
+
+            if (valor == valor2)
+            {
+                empate = empate + 1;
+                lblempate.Text = empate.ToString();
+            }
+        }
+    }
+}
